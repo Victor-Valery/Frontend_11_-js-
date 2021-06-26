caaa65eeaac405f67e830db6991a7075ca33b9d2
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-06-26 16:57:11 +0200

    js_conditional_statements

--- conditional_statement.js
+++ conditional_statement.js
@@ -90,27 +90,54 @@
 
 // Switch Case Statements
 
-var text;
-var fruits = 'APple'; //===
-fruits = fruits.toLowerCase();
+// var text;
+// var fruits2 = 'APple'; //===
+// fruits = fruits2.toLowerCase();
 
-switch (fruits) {
-    case "banana":
-        text = "Banana is good.";
-        break;
-    case "orange":
-        text = "I am not a fan of orange.";
-        break;
-    case "apple":
-        text = "How you like them apples?.";
-        break;
-    default:
-        text = "I have never heard of that fruit ..."
-}
-console.log(text);
+// switch (fruits) {
+//     case "banana":
+//         text = "Banana is good.";
+//         break;
+//     case "orange":
+//         text = "I am not a fan of orange.";
+//         break;
+//     case "apple":
+//         text = "How you like them apples?.";
+//         break;
+//     default:
+//         text = "I have never heard of that fruit ..."
+// }
+// console.log(text);
+// console.log(fruits2, text);
 
 // toLowerCase()
 
+// ************************** 
 
 
+// Bir gün degiskeni
+// Switch case pzt - cars - pers - cmt in class
+// sali ve cuma ise TW var
+// Pazar günü tatil
 
+var day = prompt("Enter a day: ");
+var day = day.toLowerCase();
+var text;
+switch (day) {
+    case "monday":
+    case "wednesday":
+    case "thursday":
+    case "saturday":
+        text = "In class";
+        break;
+    case "tuesday":
+    case "friday":
+        text = "Teamwork";
+        break;
+    case "sunday":
+        text = "Holiday";
+        break;
+    default:
+        text = "You entered wrong."
+}
+console.log(text);
